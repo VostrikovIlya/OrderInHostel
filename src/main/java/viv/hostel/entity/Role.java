43703e2c0b3af852f88882a5c168b406d1bd4467
Ilya <ilyavostrikov@icloud.com>
2022-09-24 14:32:27 +0300

    add entity user,role

--- src/main/java/viv/hostel/entity/Role.java
+++ src/main/java/viv/hostel/entity/Role.java
@@ -1,5 +1,32 @@
 package viv.hostel.entity;
 
-public enum Role {
-    USER, ADMIN;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
 }
--- src/main/java/viv/hostel/entity/User.java
+++ src/main/java/viv/hostel/entity/User.java
@@ -1,19 +1,33 @@
 package viv.hostel.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Column;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Collection;
-import java.util.List;
 
-public class User  implements UserDetails {
 
+@Getter
+@Setter
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
     private Long id;
 
-
+    @NotEmpty
+    @Size(min = 8)
     private String numberContract;
 
+    @NotEmpty
+    @Size(min = 8)
     private String password;
 
     private String firstName;
@@ -24,13 +38,21 @@ public class User  implements UserDetails {
 
     private String phone;
 
+    @ColumnDefault("true")
     private boolean enabled;
 
-    private List<Role> roles;
+    @ColumnDefault("false")
+    private boolean locked;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable( name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> authorities;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
@@ -45,17 +67,17 @@ public class User  implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
         return enabled;
     }
 
     @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return enabled;
     }
 
     @Override
