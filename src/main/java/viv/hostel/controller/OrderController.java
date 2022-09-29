d07fb2789d67cab76399faa5f23eb1c3b81af4f2
Ilya <ilyavostrikov@icloud.com>
2022-09-29 15:58:41 +0300

    create controller

---
+++ src/main/java/viv/hostel/controller/OrderController.java
@@ -0,0 +1,35 @@
+package viv.hostel.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import viv.hostel.entity.Order;
+import viv.hostel.service.OrderService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/order")
+public class OrderController {
+    private OrderService orderService;
+
+    @GetMapping("/all")
+    public List<Order> getAll() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/not-complete")
+    public List<Order> getNotComplete() {
+        return orderService.getByComplete(false);
+    }
+
+    @GetMapping("/complete")
+    public List<Order> getComplete() {
+        return orderService.getByComplete(true);
+    }
+
+    @GetMapping("/{departmentSlug}")
+    public List<Order> getOrderByDepartment(@PathVariable String departmentSlug) {
+        return orderService.getByDepartment(departmentSlug);
+    }
+}
