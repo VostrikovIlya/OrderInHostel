8465dc093448a3744458f5904aefdc80ab343765
Ilya <ilyavostrikov@icloud.com>
2022-09-29 15:58:14 +0300

    add new method, add exceptions to methods

--- src/main/java/viv/hostel/service/OrderService.java
+++ src/main/java/viv/hostel/service/OrderService.java
@@ -2,7 +2,9 @@ package viv.hostel.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import viv.hostel.entity.Department;
 import viv.hostel.entity.Order;
+import viv.hostel.repository.DepartmentRepo;
 import viv.hostel.repository.OrderRepo;
 
 import java.util.List;
@@ -10,21 +12,28 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class OrderService {
-    private OrderRepo orderRepo;
+    private final OrderRepo orderRepo;
+    private final DepartmentRepo departmentRepo;
 
     public Order getByName(String name) {
-        return orderRepo.getByName(name);
+        return orderRepo.findByName(name).orElseThrow(() -> new RuntimeException("Not found with name:" + name));
     }
 
     public List<Order> getByUserId(Long userId) {
-        return orderRepo.getByUserId(userId);
+        return orderRepo.findByUserId(userId);
     }
 
     public List<Order> getAllOrders() {
-        return orderRepo.getAll();
+        return orderRepo.findAll();
     }
 
     public List<Order> getByComplete(boolean complete) {
-        return orderRepo.getByComplete(complete);
+        return orderRepo.findByComplete(complete);
+    }
+
+    public List<Order> getByDepartment(String departmentSlug) {
+        Department depart = departmentRepo.findBySlug(departmentSlug).orElseThrow(
+                () -> new RuntimeException("Not found department with " + departmentSlug));
+        return orderRepo.findByDepartment(depart);
     }
 }
