046ab4439566ec241f7dcc50ba105d60d51f2211
Ilya <ilyavostrikov@icloud.com>
2022-11-03 17:09:53 +0300

    change secure config

--- src/main/java/viv/hostel/SecureConfig.java
+++ src/main/java/viv/hostel/SecureConfig.java
@@ -1,39 +1,45 @@
 package viv.hostel;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import viv.hostel.service.UserService;
 
-
-
+@RequiredArgsConstructor
+@Configuration
 @EnableWebSecurity
 public class SecureConfig {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .antMatchers("/api/order/*", "/api/depart/*").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .formLogin(withDefaults());
+        http.httpBasic();
+        http.authorizeHttpRequests().antMatchers("/login", "/logout").permitAll();
+        http.authorizeHttpRequests().antMatchers(HttpMethod.POST,"api/depart/save").permitAll();
+        http.authorizeHttpRequests().antMatchers("/api/depart/all").authenticated()
+                .and()
+                .formLogin().disable()
+                .csrf().disable();
         return http.build();
     }
     @Bean
-    UserDetailsService users() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user1")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    DaoAuthenticationProvider daoAuthenticationProvider()  {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
     }
+
 }
